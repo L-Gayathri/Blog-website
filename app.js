bf46de4893fe334be43ce07ebad514cfd6be234e
Lakkakula Gayathri <159698021+L-Gayathri@users.noreply.github.com>
2025-11-28 17:02:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 def factorial(n):
-    r
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
@@ -29,6 +28,7 @@ app.use('/', routes);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
 
 
 
