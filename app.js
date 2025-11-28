c4b4d650cc0529a8540aeef4296983ac99913bb1
Lakkakula Gayathri <159698021+L-Gayathri@users.noreply.github.com>
2025-11-28 17:05:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-d
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
@@ -28,6 +27,7 @@ app.use('/', routes);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
 
 
 
